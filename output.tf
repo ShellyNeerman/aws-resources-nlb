9de3108dd8fd28f645f0ee7aa58720ba22dd1938
ShellyNeerman <shelly.neerman@hashicorp.com>
2024-07-31 09:59:28 -0500

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,14 +1,11 @@
-/*
-output "alb_arn" {
-  description = "The ARN of the ALB"
-  value       = aws_lb.this.arn
- output "alb_dns_name" {
-  description = "The DNS name of the ALB"
-  value       = aws_lb.this.dns_name
+output "nlb_dns_name_output" {
+  value = aws_lb.nlb.dns_name
 }
-*/
 
-output "alb_zone_id" {
-  description = "The zone_id of the ALB"
-  value       = aws_lb.this.zone_id
+output "ec2_instance_id" {
+  value = aws_instance.web.id
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.web.public_ip
 }
